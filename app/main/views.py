b6ac49a1eb4205cf2c1cdbe4b1359c594d27b795
Alchemy <maanoatu17@gmail.com>
2018-04-19 09:15:13 +0300

    designing the templates

--- app/main/views.py
+++ app/main/views.py
@@ -6,5 +6,14 @@ from flask import Flask, render_template
 def index():
     return render_template('index.html')
 
+@main.route('/about')
+def about():
+    return render_template('about.html')
 
- 
+@main.route('/post')
+def post():
+    return render_template('post.html')
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
