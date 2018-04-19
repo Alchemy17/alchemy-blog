12d50a77b5813d845332bccdd8ee63879f471b45
Alchemy <maanoatu17@gmail.com>
2018-04-19 18:51:27 +0300

    adding necessary routes

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,7 @@ from .. import db
 @main.route('/')
 def index():
 
-    posts = Post.query.all()
+    posts = Post.query.order_by(Post.date_posted.desc()). all()
 
     return render_template('index.html', posts = posts)
 
