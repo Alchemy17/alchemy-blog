df310db4a27ade0aff41a7be8ec9932c071f7c38
Alchemy <maanoatu17@gmail.com>
2018-04-23 01:30:45 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
     SECRET_KEY = 'laziness'
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     DEBUG = True
