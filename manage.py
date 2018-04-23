1deb8e83ba6fecf3725747a28501a7696614a09b
Alchemy <maanoatu17@gmail.com>
2018-04-23 14:39:20 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app,db
 from app.models import Post, User, Comment
 from  flask_migrate import Migrate, MigrateCommand
 
-app = create_app('default')
+app = create_app('production')
 
 
 manager = Manager(app)
