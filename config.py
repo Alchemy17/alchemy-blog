a3bdbbe01ac205519b515b115d63fc4e751a4a0a
Alchemy <maanoatu17@gmail.com>
2018-04-23 15:55:18 +0300

    delete alembic

--- config.py
+++ config.py
@@ -4,11 +4,12 @@ class Config:
     SECRET_KEY = 'laziness'
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alchemy:1012@localhost/post'
-
+    
 
 config_options ={"production":ProdConfig,"default":DevConfig}
 
